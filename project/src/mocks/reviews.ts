c537cc3895c99a51c45d2e0e2e0dfd6bb0dff1ea
Dimahtml <bomabo@mail.ru>
2022-03-17 11:33:19 +0500

    добавил моки reviews и их типизацию

---
+++ project/src/mocks/reviews.ts
@@ -0,0 +1,66 @@
+import { Reviews } from '../types/reviews';
+
+const REVIEWS: Reviews = [
+  {
+    'id': 1,
+    'user': {
+      'id': 12,
+      'name': 'Isaac',
+    },
+    'rating': 2.9,
+    'comment': 'A movie that will take you to another world full of emotions.',
+    'date': '2022-02-14T21:48:13.678Z',
+  },
+  {
+    'id': 2,
+    'user': {
+      'id': 13,
+      'name': 'Zak',
+    },
+    'rating': 4.1,
+    'comment': 'A movie that will take you to another world full of emotions.',
+    'date': '2022-02-21T21:48:13.678Z',
+  },
+  {
+    'id': 3,
+    'user': {
+      'id': 13,
+      'name': 'Zak',
+    },
+    'rating': 5.3,
+    'comment': 'This movie is perfect in all its categories: credits, sound track, production, casting, writing, photography, editing, acting, and direction.\nI was amazed with the freedom of the use of the camera. This movie will change the way movies are made. Slow-mo, stills, black and white, and color were all used to brilliant effect.',
+    'date': '2022-02-14T21:48:13.678Z',
+  },
+  {
+    'id': 4,
+    'user': {
+      'id': 11,
+      'name': 'Jack',
+    },
+    'rating': 7.2,
+    'comment': 'It was well acted, directed, and the music was good. But the story is yawn. Not trying to rip anybody but I checked my watch a dozen times during this movie.',
+    'date': '2022-02-10T21:48:13.678Z',
+  },
+  {
+    'id': 5,
+    'user': {
+      'id': 10,
+      'name': 'Max',
+    },
+    'rating': 7.9,
+    'comment': 'This movie is just plain bad. There must be some big payola going round this awards season. Badly written, average acting at best, all the characters are unrelatable and inlikeable. 2 hours of my life wasted.',
+    'date': '2022-02-19T21:48:13.678Z',
+  },
+  {
+    'id': 6,
+    'user': {
+      'id': 17,
+      'name': 'Emely',
+    },
+    'rating': 9.7,
+    'comment': 'I love this movie. This film is a milestone in cinematography. Great Immersive camera-work. This film is an experience and i has already seen it 4 times and I only see more quality of the film.',
+    'date': '2022-02-06T21:48:13.678Z',
+  },
+];
+
+export { REVIEWS };
