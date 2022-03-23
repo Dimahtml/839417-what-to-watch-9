1a70f3e47c11b1ed2e9c0364af80b6a16fd486ce
Dimahtml <bomabo@mail.ru>
2022-03-23 10:04:38 +0500

    работает ShowMoreBtn

--- project/src/components/pages/main-screen/main-screen.tsx
+++ project/src/components/pages/main-screen/main-screen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, MouseEventHandler } from 'react';
 import { useAppSelector } from '../../../hooks';
 import { getFilmsByActiveGenre } from '../../../store/selectors';
 
@@ -7,10 +7,22 @@ import FilmPromo from './film-promo/film-promo';
 import PageFooter from '../../page-footer/page-footer';
 import GenresList from './genres-list/genres-list';
 import ShowMoreBtn from './show-more-btn/show-more-btn';
+import { Film } from '../../../types/films';
+import { FILMS_PER_STEP } from '../../../const';
 
 function MainScreen(): JSX.Element {
+  const genre = useAppSelector((state) => state.genre);
   const films = useAppSelector((state) => state.films);
   const activeFilms = useAppSelector(getFilmsByActiveGenre);
+  const [showedFilms, setShowedFilms] = useState<Film[]>(activeFilms.slice(0, FILMS_PER_STEP));
+
+  useEffect(() => {
+    setShowedFilms(activeFilms.slice(0, FILMS_PER_STEP));
+  }, [films, activeFilms, genre]);
+
+  const onShowMoreBtnClick: MouseEventHandler<HTMLButtonElement> = () => {
+    setShowedFilms(activeFilms.slice(0, showedFilms.length + FILMS_PER_STEP));
+  };
 
   return (
     <React.Fragment>
@@ -20,8 +32,9 @@ function MainScreen(): JSX.Element {
         <section className="catalog">
           <h2 className="catalog__title visually-hidden">Catalog</h2>
           <GenresList films={films} />
-          <FilmsList films={activeFilms} />
-          <ShowMoreBtn />
+          <FilmsList films={showedFilms} />
+          {showedFilms.length < activeFilms.length
+          && <ShowMoreBtn onShowMoreBtnClick={onShowMoreBtnClick} />}
         </section>
         <PageFooter />
       </div>
--- project/src/components/pages/main-screen/show-more-btn/show-more-btn.tsx
+++ project/src/components/pages/main-screen/show-more-btn/show-more-btn.tsx
@@ -1,7 +1,17 @@
-function ShowMoreBtn(): JSX.Element {
+type ShowMoreBtnProps = {
+  onShowMoreBtnClick: React.MouseEventHandler<HTMLButtonElement>;
+};
+
+function ShowMoreBtn({onShowMoreBtnClick}: ShowMoreBtnProps): JSX.Element {
   return (
     <div className="catalog__more">
-      <button className="catalog__button" type="button">Show more</button>
+      <button
+        className="catalog__button"
+        type="button"
+        onClick={onShowMoreBtnClick}
+      >
+        Show more
+      </button>
     </div>
   );
 }
